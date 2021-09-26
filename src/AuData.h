45787333b334506a541dd939a078311ed78167c0
Nixy C <phoenixrising1800@gmail.com>
2021-09-26 02:09:55 -0400

    template3

--- src/AuData.h
+++ src/AuData.h
@@ -26,7 +26,7 @@
 
 // AuMuLawAudioData
 typedef AudioDataImpl<AuMuLaw,BYTE> AuMuLawAudioData ;
-inline BYTE AuMuLawAudioData::readValue (BinaryIO* io) const { return (io->read8()) ; }
+template <typename T> inline BYTE AuMuLawAudioData::readValue (BinaryIO* io) const { return (io->read8()) ; }
 inline void AuMuLawAudioData::writeValue (BinaryIO* io, BYTE v) const { io->write8(v) ; }
 
 // AuPCM8AudioData
